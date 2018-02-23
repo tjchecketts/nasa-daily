8e1ce7dadfb7de17dfa1d6ddcfac913bb6745081
“TJ <tylerchecketts@gmail.com>
2018-02-23 11:28:01 -0700

    set resp data to state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import React, { Component } from 'react';
-import './App.css';
-import axios from 'axios';
+import React, { Component } from 'react'
+import axios from 'axios'
 
 const url = "https://api.nasa.gov/planetary/apod?api_key=kkJChhhtZ46bGlDb42QYtu97SOjNsoGIIVXdXio6"
 
 class App extends Component {
-  componentDidMount = () => {
-    axios.get(url).then(function (resp) {
-      console.log(resp)
-    })
+  state = { data: {} }
+
+  componentWillMount = () => {
+    axios.get(url)
+      .then( resp => this.setState({ data: resp.data }) )
     .catch(function (err) {
       console.log(err)
     })
-  };
+  }
   
   render() {
+    let { 
+      data: {
+        date, 
+        explanation, 
+        hdurl, 
+        media_type, 
+        service_version, 
+        title, 
+        url
+      } 
+    } = this.state
+
+    const dataText = Object.entries(this.state.data)
+      .map(([key, value]) => {
+        return (
+          <div key={key}>
+            <div style={{ fontWeight: 'bold', fontSize: 20 }}>
+              {key}:
+            </div>
+            <div>
+              {value}
+            </div>
+          </div>
+        )
+      })
+    
     return (
       <div>
-        hey
+        <div>
+          {dataText}
+        </div>
+        <img src={url} />
       </div>
-    );
+    )
   }
 }
 
