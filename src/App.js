d3ea4525682bd5a53d7e6208830bd2efeab26961
“TJ <tylerchecketts@gmail.com>
2018-02-22 20:59:05 -0700

    add axios and get data on mount

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+const url = "https://api.nasa.gov/planetary/apod?api_key=kkJChhhtZ46bGlDb42QYtu97SOjNsoGIIVXdXio6"
 
 class App extends Component {
+  componentDidMount = () => {
+    axios.get(url).then(function (resp) {
+      console.log(resp)
+    })
+    .catch(function (err) {
+      console.log(err)
+    })
+  };
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        hey
       </div>
     );
   }
